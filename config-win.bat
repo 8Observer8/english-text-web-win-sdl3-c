08603a3a7ad3b289ae8551f330a8f49d7ce108e7
Ivan Enzhaev <8observer8@gmail.com>
2025-01-05 03:16:03 +0400

    Initial commit

---
+++ config-win.bat
@@ -0,0 +1,1 @@
+cmake -G "MinGW Makefiles" -S . -B dist/win -DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/win/lib/cmake/SDL3 -DSDL3_ttf_DIR=E:/libs/sdl3_ttf-3.0.0-prefix/win/lib/cmake/SDL3_ttf -DFREETYPE_INCLUDE_DIRS=E:/libs/freetype-2-13-3-prefix/win/include -DFREETYPE_LIBRARY=E:/libs/freetype-2-13-3-prefix/win/lib/libfreetype.a -DCMAKE_BUILD_TYPE=Debug
---
+++ src/main.c
@@ -0,0 +1,82 @@
+#define SDL_MAIN_USE_CALLBACKS 1 // Use the callbacks instead of main()
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+// We will use this renderer to draw into this window every frame
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+static SDL_Texture *texture = NULL;
+
+// This function runs once at startup
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Initialize the TTF library
+    if (!TTF_Init()) {
+        SDL_Log("Couldn't initialize TTF: %s",SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("English text using SDL3 and C",
+        500, 500, 0, &window, &renderer)) //
+    {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    TTF_Font *font = TTF_OpenFont("assets/fonts/arial.ttf", 48);
+    SDL_Color textColor = (SDL_Color){255, 255, 255};
+    SDL_Surface *surface = TTF_RenderText_Blended(font, "Hello, World!", 13, textColor);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_DestroySurface(surface);
+
+    return SDL_APP_CONTINUE; // Carry on with the program
+}
+
+// This function runs when a new event (mouse input, keypresses, etc) occurs
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS; // End the program, reporting success to the OS
+    }
+    return SDL_APP_CONTINUE; // Carry on with the program
+}
+
+// This function runs once per frame, and is the heart of the program
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    // Set the position where you want to draw the text
+    float x = 10.f;
+    float y = 10.f;
+
+    // Create a rectangle to hold the texture dimensions
+    SDL_FRect rect;
+    SDL_GetTextureSize(texture, &rect.w, &rect.h);
+    rect.x = x;
+    rect.y = y;
+
+    // SDL_RenderTexture(renderer, texture, NULL, NULL);
+    // Clear the screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw the text
+    SDL_RenderTexture(renderer, texture, NULL, &rect);
+
+    // Update the screen
+    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+// This function runs once at shutdown
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    // SDL will clean up the window/renderer for us
+    TTF_Quit();
+    SDL_Quit();
+}
