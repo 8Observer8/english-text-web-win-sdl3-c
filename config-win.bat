c14ae51a7772dee6d609e44dbedd3f5b31c49f85
Ivan Enzhaev <8observer8@gmail.com>
2025-09-20 21:52:46 +0400

    Formated

--- config-win.bat
+++ config-win.bat
@@ -1,1 +1,6 @@
-cmake -G "MinGW Makefiles" -S . -B dist/win -DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/win/lib/cmake/SDL3 -DSDL3_ttf_DIR=E:/libs/sdl3_ttf-3.0.0-prefix/win/lib/cmake/SDL3_ttf -DFREETYPE_INCLUDE_DIRS=E:/libs/freetype-2-13-3-prefix/win/include -DFREETYPE_LIBRARY=E:/libs/freetype-2-13-3-prefix/win/lib/libfreetype.a -DCMAKE_BUILD_TYPE=Debug
+cmake -G "MinGW Makefiles" -S . -B dist/win ^
+-DSDL3_DIR=H:/libs/sdl-3.1.6-prefix/win/lib/cmake/SDL3 ^
+-DSDL3_ttf_DIR=H:/libs/sdl3_ttf-3.0.0-prefix/win/lib/cmake/SDL3_ttf ^
+-DFREETYPE_INCLUDE_DIRS=H:/libs/freetype-2.13.3-prefix/win/include ^
+-DFREETYPE_LIBRARY=H:/libs/freetype-2.13.3-prefix/win/lib/libfreetype.a ^
+-DCMAKE_BUILD_TYPE=Debug
