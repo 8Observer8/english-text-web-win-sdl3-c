89ad61e7a8c06283a347eb2a7143ef3bca219e42
Ivan Enzhaev <8observer8@gmail.com>
2025-12-09 23:59:11 +0400

    Clean up

--- config-web.bat
+++ config-web.bat
@@ -4,4 +4,3 @@ emcmake cmake -S . -B dist/web ^
 -DFREETYPE_INCLUDE_DIRS=H:/libs/freetype-2.13.3-prefix/web/include ^
 -DFREETYPE_LIBRARY=H:/libs/freetype-2.13.3-prefix/web/lib/libfreetype.a ^
 -DCMAKE_BUILD_TYPE=Debug
-emcmake cmake -G "MinGW Makefiles" -S . -B dist/web -DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/web/lib/cmake/SDL3 -DSDL3_ttf_DIR=E:/libs/sdl3_ttf-3.0.0-prefix/web/lib/cmake/SDL3_ttf -DFREETYPE_INCLUDE_DIRS=E:/libs/freetype-2-13-3-prefix/web/include -DFREETYPE_LIBRARY=E:/libs/freetype-2-13-3-prefix/web/lib/libfreetype.a -DCMAKE_BUILD_TYPE=Debug
