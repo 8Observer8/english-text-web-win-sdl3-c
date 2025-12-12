e648a57103a5559a24c39dedaf9552f134e8b0d5
Ivan Enzhaev <8observer8@gmail.com>
2025-12-12 21:17:24 +0400

    Add a free LiberationSans-Regular.ttf file

--- src/main.c
+++ src/main.c
@@ -11,40 +11,43 @@ static SDL_Texture *texture = NULL;
 // This function runs once at startup
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
     // Initialize the TTF library
-    if (!TTF_Init()) {
-        SDL_Log("Couldn't initialize TTF: %s",SDL_GetError());
+    if (!TTF_Init())
+    {
+        SDL_Log("Couldn't initialize TTF: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
     if (!SDL_CreateWindowAndRenderer("English text using SDL3 and C",
-        500, 500, 0, &window, &renderer)) //
+            500, 500, 0, &window, &renderer))
     {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    TTF_Font *font = TTF_OpenFont("assets/fonts/arial.ttf", 48);
-    SDL_Color textColor = (SDL_Color){255, 255, 255};
-    SDL_Surface *surface = TTF_RenderText_Blended(font, "Hello, World!", 13, textColor);
+    TTF_Font *font = TTF_OpenFont("assets/fonts/LiberationSans-Regular.ttf", 48);
+    SDL_Color textColor = (SDL_Color) { 255, 255, 255 };
+    SDL_Surface *surface = TTF_RenderText_Blended(font, "Hello World!", 13, textColor);
     texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_DestroySurface(surface);
 
-    return SDL_APP_CONTINUE; // Carry on with the program
+    return SDL_APP_CONTINUE;
 }
 
 // This function runs when a new event (mouse input, keypresses, etc) occurs
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_QUIT) {
+    if (event->type == SDL_EVENT_QUIT)
+    {
         return SDL_APP_SUCCESS; // End the program, reporting success to the OS
     }
-    return SDL_APP_CONTINUE; // Carry on with the program
+    return SDL_APP_CONTINUE;
 }
 
 // This function runs once per frame, and is the heart of the program
@@ -70,13 +73,24 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
     // Update the screen
     SDL_RenderPresent(renderer);
-    return SDL_APP_CONTINUE;  /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
 // This function runs once at shutdown
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    // SDL will clean up the window/renderer for us
+    if (renderer)
+    {
+        SDL_DestroyRenderer(renderer);
+        renderer = NULL;
+    }
+
+    if (window)
+    {
+        SDL_DestroyWindow(window);
+        window = NULL;
+    }
+
     TTF_Quit();
     SDL_Quit();
 }
